bb8f24776f1b2ab46a75603f36033cda79180ba1
mingmoth <mingmoth@gmail.com>
2024-06-21 18:32:04 +0800

    feat: add filterTodos function for display

--- todo-list/src/TodoApp.jsx
+++ todo-list/src/TodoApp.jsx
@@ -2,9 +2,22 @@ import { useState } from 'react'
 import Todo from './components/Todo'
 // import './todoApp.css'
 
+const filterOptions = ['all', 'active', 'completed']
+const filterTodos = (todos, filter) => {
+    switch (filter) {
+        case 'active':
+            return todos.filter(todo => !todo.checked)
+        case 'completed':
+            return todos.filter(todo => todo.checked)
+        default:
+            return todos
+    }
+}
+
 function TodoApp() {
 	const [todos, setTodos] = useState([])
 	const [newTodo, setNewTodo] = useState('')
+    const [filter, setFilter] = useState('all')
 
 	function addTodo() {
         if (!newTodo) return
@@ -70,14 +83,14 @@ function TodoApp() {
 				<button onClick={addTodo} >Add</button>
 			</div>
             <div style={{textAlign: 'start'}}>
-                <select>
-                    <option value="all">All</option>
-                    <option value="active">Active</option>
-                    <option value="completed">Completed</option>
+                <select value={filter} onChange={e => setFilter(e.target.value)}>
+                    {filterOptions.map(option => (
+                        <option key={option} value={option}>{option}</option>
+                    ))}
                 </select>
             </div>
 			<div className='todo-container'>
-				{todos.map((todo) => (
+				{filterTodos(todos, filter).map((todo) => (
 					<Todo key={todo.id} todo={todo} setTodos={setTodos} onToggleTodo={onToggleTodo} deleteTodo={deleteTodo} onToggleTodoEditing={onToggleTodoEditing} />
 				))}
 			</div>
