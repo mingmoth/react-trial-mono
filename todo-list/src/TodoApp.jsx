6e2a863deb62f05a2fb237c432b909b8c6323a9a
mingmoth <mingmoth@gmail.com>
2024-06-21 22:40:24 +0800

    refactor: extract AddTodo component

--- todo-list/src/TodoApp.jsx
+++ todo-list/src/TodoApp.jsx
@@ -17,35 +17,13 @@ const filterTodos = (todos, filter) => {
 
 function TodoApp() {
     const [todos, dispatch] = useReducer(todoReducer, [])
-	const [newTodo, setNewTodo] = useState('')
     const [filter, setFilter] = useState('all')
-
-	function addTodo() {
-        if (!newTodo) return
-        dispatch({
-            type: 'add',
-            todo: {
-                id: Date.now(),
-				checked: false,
-				name: newTodo,
-                editing: false,
-            }
-        })
-		setNewTodo('')
-	}
-
-	function handleKeyEnter(e) {
-		if (e.key === 'Enter') {
-			addTodo()
-		}
-	}
 
 	return (
 		<>
 			<h1>todos</h1>
 			<div style={{display: 'flex', gap: '8px', alignItems: 'center', width: '100%'}}>
-				<input type='text' value={newTodo} onChange={(e) => setNewTodo(e.target.value)} onKeyDown={handleKeyEnter} style={{flex: 1}} />
-				<button onClick={addTodo} >Add</button>
+				<AddTodo dispatch={dispatch} />
 			</div>
             <div style={{textAlign: 'start'}}>
                 <select value={filter} onChange={e => setFilter(e.target.value)}>
@@ -61,6 +39,36 @@ function TodoApp() {
 			</div>
 		</>
 	)
+}
+
+function AddTodo({ dispatch }) {
+	const [newTodo, setNewTodo] = useState('')
+
+    function addTodo() {
+        if (!newTodo) return
+        dispatch({
+            type: 'add',
+            todo: {
+                id: Date.now(),
+				checked: false,
+				name: newTodo,
+                editing: false,
+            }
+        })
+		setNewTodo('')
+	}
+
+
+    function handleKeyEnter(e) {
+		if (e.key === 'Enter') {
+			addTodo()
+		}
+	}
+
+    return <>
+        <input type='text' value={newTodo} onChange={(e) => setNewTodo(e.target.value)} onKeyDown={handleKeyEnter} style={{flex: 1}} />
+        <button onClick={addTodo} >Add</button>
+    </>
 }
 
 export default TodoApp
