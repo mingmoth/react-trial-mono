cb5ae4b8a1bcfb495a47cb30c81c0f5786efe523
mingmoth <mingmoth@gmail.com>
2024-06-22 20:40:42 +0800

    feat: add synonyms functionality

--- simple-tab/src/practices/Synonym/index.jsx
+++ simple-tab/src/practices/Synonym/index.jsx
@@ -1,15 +1,34 @@
 import "./styles.css";
 
 import React, { useState } from "react";
+import useFetch from "./useFetch";
+
+const url = 'https://api.datamuse.com/words?ml='
 
 const App = () => {
     const [word, setWord] = useState("");
+    const {
+        data: results, loading, fetchUrl
+    } = useFetch()
     // TODO: Declare your state variables for holding synonyms and loading state
 
-    const handleSubmit = (e) => {
+    async function getSynonyms(search) {
+        if(!search) return
+        const searchWord = search.trim().split(' ').join('+')
+        fetchUrl(`${url}${searchWord}`)
+    }
+
+    const handleSubmit = async (e) => {
         e.preventDefault();
         // TODO: Implement fetching synonyms
+        await getSynonyms(word)
     };
+
+    async function handleClickItem(search) {
+        setWord(search)
+        await getSynonyms(search)
+
+    }
 
     return (
         <div className="App">
@@ -21,9 +40,18 @@ const App = () => {
                     onChange={(e) => setWord(e.target.value)}
                     placeholder="Enter a word"
                 />
-                <button type="submit">Find Synonyms</button>
+                <button type="submit" onClick={handleSubmit}>Find Synonyms</button>
             </form>
             {/* TODO: Display loading state or fetched synonyms */}
+            {loading && <p>...Loading</p>}
+            {results.length > 0 && (
+                <ul>
+                    {results.map(result => (
+                        <li key={result.word} onClick={(() => handleClickItem(result.word))}>{result.word}</li>
+                    ))}
+                </ul>
+            )}
+            {results.length < 1 && !loading && <p>No results found</p>}
         </div>
     );
 };
