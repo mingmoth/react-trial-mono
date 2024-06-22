a126e7fcce2199372d857dd6a9401c2513b8029d
mingmoth <mingmoth@gmail.com>
2024-06-22 18:40:58 +0800

    feat: add beating functionality

--- simple-tab/src/practices/Emoji/index.jsx
+++ simple-tab/src/practices/Emoji/index.jsx
@@ -1,16 +1,32 @@
+import { useEffect, useState } from "react";
 import "./styles.css";
-import { useState } from 'react'
 
-function Emoji({ emoji }) {
+function sleep(delay = 1000) {
+    return new Promise((resolve) => setTimeout(resolve, delay))
+}
+
+function Emoji({ emoji, isBeating }) {
+    const [start, setStart] = useState(false)
+
+    async function waitBeating() {
+        await sleep(2000)
+        setStart(true)
+    }
+
+    useEffect(() => {
+        waitBeating()
+    }, [])
+
     return (
         <span
             key={emoji.id}
             img='role'
-            className="emoji"
+            className={`emoji ${(isBeating && start) ? 'heartbeat' : ''}`}
             style={{
                 position: 'absolute',
                 top: `${emoji.y - 36}px`,
                 left: `${emoji.x - 18}px`,
+
             }}
         >{emoji.icon}</span>
     )
@@ -19,6 +35,7 @@ function Emoji({ emoji }) {
 function App() {
     const fruitEmojis = ["🍎", "🍊", "🍇", "🍓", "🍒"];
     const [emojis, setEmojis] = useState([])
+    const [isBeating, setIsBeating] = useState(true)
 
     function getEmojis() {
         const randomEmojiIndex = Math.floor(Math.random() * fruitEmojis.length)
@@ -28,8 +45,6 @@ function App() {
     function handleClick(e) {
         const eventX = e.clientX
         const eventY = e.clientY
-        console.log('x', e.clientX)
-        console.log('y', e.clientY)
         setEmojis(oldEmojis => [...oldEmojis, {
             id: Date.now(),
             x: eventX,
@@ -38,14 +53,19 @@ function App() {
         }])
     }
 
+    function handleButtonClick(e) {
+        e.stopPropagation()
+        setIsBeating(oldBeating => !oldBeating)
+    }
+
     return (
         <div
             style={{ width: "100vw", height: "100vh", position: "relative" }}
             onClick={handleClick}
         >
             <h1>Click to add an fruit emoji 🍎🍊🍇</h1>
-            // Display your emojis
-            {emojis.map(emoji => <Emoji key={emoji.id} emoji={emoji} />)}
+            <button onClick={handleButtonClick}>Click me to toggle animation</button>
+            {emojis.map(emoji => <Emoji key={emoji.id} emoji={emoji} isBeating={isBeating} />)}
         </div>
     );
 }
