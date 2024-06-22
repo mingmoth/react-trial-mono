5621893f980baaa58bbb4762ea02393e216857ca
mingmoth <mingmoth@gmail.com>
2024-06-22 14:07:14 +0800

    feat: add TodoProvider in top level component App

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -1,14 +1,15 @@
 import TodoApp from './TodoApp'
+import { TodoProvider } from './TodoContext'
 import './App.css'
 
 function App() {
 
 	return (
-		<>
+		<TodoProvider>
 			<div className='todoapp'>
 				<TodoApp />
 			</div>
-		</>
+		</TodoProvider>
 	)
 }
 
