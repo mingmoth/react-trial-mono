44f5f608700282a144dd77134de8364d1571f42d
mingmoth <mingmoth@gmail.com>
2024-07-04 09:22:12 +0800

    feat: extract tic-tac-tie function to useGame hook

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -1,94 +1,9 @@
-import React, { useState } from "react";
+import useGame from './useGame';
 import "./styles.css";
-import { useEffect } from "react";
-
-function getEmptyBoard() {
-    return Array(9).fill(null)
-}
-
-const winConditions = [
-    [0, 1, 2],
-    [0, 3, 6],
-    [0, 4, 8],
-    [1, 4, 7],
-    [2, 5, 8],
-    [2, 4, 6],
-    [3, 4, 5],
-    [6, 7, 8],
-]
 
 const App = () => {
     // Initialize board with null values
-    const [board, setBoard] = useState(getEmptyBoard());
-    const [isXNext, setIsXNext] = useState(true);
-    const [isGameEnd, setIsGameEnd] = useState(false)
-    const [winner, setWinner] = useState(null)
-
-    function resetGame() {
-        setBoard(getEmptyBoard())
-        setIsGameEnd(false)
-        setIsXNext(true)
-        setWinner(null)
-    }
-
-    // TODO: Function to handle click on a cell
-    const handleClick = (index) => {
-        if(board[index] || isGameEnd) return
-        // Implement your code here
-        const newBoard = board.map((cell, idx) => {
-            if(!cell && idx === index) {
-                return cell = isXNext ? 'X' : 'O'
-            }
-            return cell
-        })
-        setBoard(newBoard)
-        setIsXNext(!isXNext)
-    };
-
-    function checkAllOccupy() {
-        return board.find(cell => cell === null) === undefined
-    }
-
-    function checkWin(player) {
-        const playerIndexArray = board.reduce((acc, cell, index) => {
-            if(cell === player) {
-                acc.push(index)
-            }
-            return acc
-        }, [])
-        return winConditions.some(condition => {
-            return condition.every(target => {
-                return playerIndexArray.includes(target)
-            })
-        })
-    }
-
-    function checkGame() {
-        if(checkWin('O')) {
-            setWinner('O')
-            setIsGameEnd(true)
-        }
-        if(checkWin('X')) {
-            setWinner('X')
-            setIsGameEnd(true)
-        }
-        if(checkAllOccupy()) {
-            setIsGameEnd(true)
-        }
-    }
-
-    function gameDisplayHint() {
-        if(isGameEnd) {
-            return winner
-                ? `Winner: ${isXNext ? 'O' : 'X'}`
-                : 'No Winner'
-        }
-        return `Next Player: ${isXNext ? 'X' : 'O'}`
-    }
-
-    useEffect(() => {
-        checkGame()
-    }, [board])
+    const { board, gameDisplayHint, handleCellClick, resetGame } = useGame();
 
     return (
         <div className="App">
@@ -96,7 +11,7 @@ const App = () => {
             <h3>{gameDisplayHint()}</h3>
             <div className="board">
                 {board.map((cell, index) => (
-                    <div className="cell" key={index} onClick={() => handleClick(index)}>
+                    <div className="cell" key={index} onClick={() => handleCellClick(index)}>
                         {cell}
                     </div>
                 ))}
